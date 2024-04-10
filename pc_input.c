2e70458cd3ce777a8ef29536aafbfd0503e83227
Winter d'Atne <104174438+WinterAtne@users.noreply.github.com>
2024-04-10 04:02:51 -0600

    Made input parsing more reliable

--- pc_input.c
+++ pc_input.c
@@ -7,9 +7,16 @@
 int framenum = 0;
 short pc_input(Board* board, char* action) {
 	char command[5] = "";
-	fgets(command, 5, stdin);
+	fgets(command, sizeof(command), stdin);
 	while (getchar() != '\n') { } 
 	*action = command[0];
-	return XYToAbsolute(board, command[2] - 'A', command[3] - 'A');
+
+	command[2] -= 'A';
+	command[3] -= 'A';
+	if (command[2] < 0 || command[3] < 0 || command[2] >= board->sizeX || command[3] >= board->sizeY) {
+		printf("invalid input, please enter valid input\n");
+		return pc_input(board, action);
+	}
+	return XYToAbsolute(board, command[2], command[3]);
 }
 
